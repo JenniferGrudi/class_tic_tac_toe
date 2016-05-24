be2710713e2b80f153c4928f31b88571be8878cf
jennifer Grudi <jen.grudi@gmail.com>
2016-05-23 20:11:13 -0400

    no human game successful

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'board_logic.rb'
 require_relative 'sequential_ai.rb'
 require_relative 'simple_ai.rb'
 require_relative 'AI_game.rb'
-require_relative 'console_game.rb'
+require_relative 'human.rb'
 
 
 enable :sessions
@@ -20,39 +20,52 @@ end
 
 post '/player_one_marker' do
 	p1_marker = params[:p1_marker]
-	# p2_marker = params[:p2_marker]
+	p2_marker = params[:p2_marker]
+	session[:board] = params[:board]
 
 		if p1_marker == "X"
-		   player_one_marker = "X"
-		   player_two_marker = "O"
+		   p2_marker = "O"
 		else
-		    player_one_marker = "O"
-			player_two_marker = "X"
+			p2_marker = "X"
 		end	
 
 		if session[:one] == "Simple AI"
-			player_one = {:player_mode => Simple.new, :marker => player_one_marker}
+			player_one = {:player_mode => Simple.new, :marker => p1_marker}
 		else session[:one] == "Sequential AI"
-			player_one = {:player_mode => Sequential.new, :marker => player_one_marker}
+			player_one = {:player_mode => Sequential.new, :marker => p1_marker}
 		end
 
 		if session[:two] == "Simple AI"
-			player_two = {:player_mode => Simple.new, :marker => player_two_marker}
+			player_two = {:player_mode => Simple.new, :marker => p2_marker}
 		else session[:two] == "Sequential AI"
-			player_two = {:player_mode => Sequential.new, :marker => player_two_marker}
+			player_two = {:player_mode => Sequential.new, :marker => p2_marker}
+		end	
+
+ session[:board] = create_new_board
+ session[:board] = play_game(player_one, player_two, session[:board])
+
+ erb :play_game, :locals => {:p1_marker => p1_marker, :p2_marker => p2_marker,:player_one => session[:one], :player_two => session[:two], :board => session[:board]}
+
+human = params[:human]
+
+		if session[:one] == "Human" && session[:two] == "Human"
+			player_one = {:player_mode => Human.new, :marker => p1_marker}
+			player_two = {:player_mode => Human.new, :marker => p2_marker}
+		elsif
+			session[:one] == "Human" && session[:two] == "Simple_AI"
+			player_one = {:player_mode => Human.new, :marker => p1_marker}
+			player_two = {:player_mode => Simple.new, :marker => p2_marker}
+		else
+			session[:one] == "Human" && session[:two] == "Sequential"
+			player_one = {:player_mode => Human.new, :marker => p1_marker}
+			player_two = {:player_mode => Sequential.new, :marker => p2_marker} 			
 		end
 
-	board = create_new_board
-	board = play_game(player_one, player_two, board)
+ erb :human_game, :locals => {:p1_marker => p1_marker, :p2_marker => p2_marker,:player_one => session[:one], :player_two => session[:two], :board => session[:board], :human => human}	
 
-	erb :play_game, :locals => {:player_one_marker => player_one_marker, :player_two_marker => player_two_marker,:player_one => session[:one], :player_two => session[:two], :board => board}
-end	
 
-post '/human' do
-	human = params[:human]
-	if player_one 
 
-	erb :human_game, :locals => {:player_one_marker => player_one_marker, :player_two_marker => player_two_marker,:player_one => session[:one], :player_two => session[:two], :board => board}
+
+
 end
-
 
